1155843119a5bd76c6a7d482553002a790958c1a
GibranEtiel <gib.beiza@duocuc.cl>
2024-07-12 02:26:52 +0200

    add

--- assent/js/script.js
+++ assent/js/script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const cards = document.querySelectorAll('.card');
     cards.forEach(card => {
         card.addEventListener('click', () => {
-            alert('¡Feliz Aniversario! Te Amo mucho 💖');
+            alert('¡Feliz Aniversario! Te amo mi Niño💖');
         });
     });
 });
